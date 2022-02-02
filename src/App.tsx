7af5774613a08b090a35891d992659cc5ccca22e
Zubair Abubakar <zuby52@gmail.com>
2022-02-02 20:14:26 +0000

    Added cart and cartItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//components
+import { Drawer } from '@mui/material';
+import { LinearProgress } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import Badge from '@mui/material/Badge';
+
+//Styles
+import { Wrapper, StyledButton } from './App.style';
+import Item from './Item/Item';
+import CartItem from './CartItem/CartItem';
+import Cart from './Cart/Cart';
+
+export type CartItemType = {
+    id: number;
+    category: string;
+    description: string;
+    image: string;
+    price: number;
+    title: string;
+    amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+    await (await fetch('https://fakestoreapi.com/products')).json();
+
+const App = () => {
+    const [cartOpen, setCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([] as CartItemType[]);
+
+    const { data, isLoading, error } = useQuery<CartItemType[]>(
+        'products',
+        getProducts
+    );
+
+    console.log(data);
+
+    const getTotalItems = (items: CartItemType[]) =>
+        items.reduce((ack: number, item) => ack + item.amount, 0);
+
+    const handleAddToCart = (clickedItem: CartItemType) => {
+        setCartItems((previousItems) => {
+            //check if item is already in the cart
+            const isItemInCart = previousItems.find(
+                (item) => item.id === clickedItem.id
+            );
+
+            if (isItemInCart) {
+                return previousItems.map((item) =>
+                    item.id === clickedItem.id
+                        ? { ...item, amount: item.amount + 1 }
+                        : item
+                );
+            }
+
+            //otherwise, add the item for the first time
+            return [...previousItems, { ...clickedItem, amount: 1 }];
+        });
+    };
+
+    const handleRemoveFromCart = (id: number) => {
+        setCartItems((previousItems) =>
+            previousItems.reduce((ack, item) => {
+                if (item.id === id) {
+                    if (item.amount === 1) return ack;
+                    return [...ack, { ...item, amount: item.amount - 1 }];
+                } else {
+                    return [...ack, item];
+                }
+            }, [] as CartItemType[])
+        );
+    };
+
+    if (isLoading) return <LinearProgress />;
+    if (error) return <div>Something went wrong ...</div>;
+
+    return (
+        <Wrapper>
+            <Drawer
+                anchor="right"
+                open={cartOpen}
+                onClose={() => setCartOpen(false)}
+            >
+                <Cart
+                    cartItems={cartItems}
+                    addToCart={handleAddToCart}
+                    removeFromCart={handleRemoveFromCart}
+                />
+            </Drawer>
+            <StyledButton onClick={() => setCartOpen(true)}>
+                <Badge badgeContent={getTotalItems(cartItems)} color="error">
+                    <AddShoppingCartIcon />
+                </Badge>
+            </StyledButton>
+            <Grid container spacing={3}>
+                {data?.map((item) => (
+                    <Grid item key={item.id} xs={12} sm={4}>
+                        <Item item={item} handleAddToCart={handleAddToCart} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Wrapper>
+    );
+};
 
 export default App;
